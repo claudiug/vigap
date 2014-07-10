b0eec9c8b3b83499c008eb00549366d0fc9ea0e6
claudiu <claudiu.garba@gmail.com>
2014-07-10 15:45:38 +0200

    add login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "logged"
+    else
+      flash.now.alert ="email or password are not match"
+    end
+  end
+
+  def destroy
+  session[:user_id] = nil
+  redirect_to root_url, notice: "Logged out!"
+  end
 end
