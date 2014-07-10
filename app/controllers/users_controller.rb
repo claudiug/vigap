272a5b65b269199710635c35788a98ce2ce618d1
claudiu <claudiu.garba@gmail.com>
2014-07-10 15:39:02 +0200

    add session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: "well done"
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
+  resources :sessions
 end
