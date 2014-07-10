a64e6834458cbbc15984320d9d6d8cdd14ab820e
claudiu <claudiu.garba@gmail.com>
2014-07-10 12:52:13 +0200

    first user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :email, presence: true, uniqueness: true, format: {with: EMAIL_REGEX}
+
+  def username
+    "#{first_name.capitalize} #{last_name.capitalize}" if first_name.present? || last_name.present?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,16 +12,19 @@ describe User do
                         email: "email@example.net", password: "qwe123")
     user1 = User.new(first_name: "demo", last_name: "demo1",
                         email: "email@example.net", password: "qwe123")
-    expect(user).to have(1).errors_on(:email)
+    expect(user1).to have(1).errors_on(:email)
   end
 
   it "is invalid with no email" do
     user = User.new(email: nil)
-    expect(user).to have(1).errors_on(:email)
+    expect(user).to have(2).errors_on(:email)
   end
 
   it "return the user name made by first_name + last_name" do
+    user = User.create!(first_name: "demo", last_name: "demo1",
+                        email: "email@example.net", password: "qwe123")
 
+    expect(user.username).to eq "Demo Demo1"
   end
 
   it "return the username in form as email when first_name and last_name are nil" do
