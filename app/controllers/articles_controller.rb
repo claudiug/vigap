b8532ec69a6e7a334d929afa26cef7f25da85f3f
claudiu <claudiu.garba@gmail.com>
2014-07-11 15:06:25 +0200

    add basic form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,45 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+    render json: @articles
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save!
+      redirect_to root_url, notice: "article created"
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comments = @article.comments
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(article_params)
+    redirect_to root_url if @article.destroy
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 ##USERS
 User.delete_all
-user = User.create(username: "demo123", first_name: "c", last_name: "d", password:"1234567",
+user = User.create!(username: "demo123", first_name: "c", last_name: "d", password:"1234567",
                     email: "email@example.net")
 ##ARTICLES
 Article.delete_all
