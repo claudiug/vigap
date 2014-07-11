fe323938bf10001aa7be20e8077d1bb1ee82a9f2
claudiu <claudiu.garba@gmail.com>
2014-07-11 15:43:13 +0200

    move to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140711102740) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
